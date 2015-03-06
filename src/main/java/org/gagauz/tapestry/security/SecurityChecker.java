bf60538088e867116a82be70bd2553f8662e2f59
Michail Gagauz <michael.gagauz@kupivip.ru>
2015-03-06 19:00:24 +0300

    Refactoring

--- src/main/java/org/gagauz/tapestry/security/SecurityChecker.java
+++ src/main/java/org/gagauz/tapestry/security/SecurityChecker.java
@@ -1,18 +1,17 @@
 package org.gagauz.tapestry.security;
 
+import org.apache.tapestry5.annotations.SessionState;
 import org.gagauz.tapestry.security.api.SecurityUser;
-
-import javax.inject.Inject;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class SecurityChecker.
  */
 public class SecurityChecker {
-    
+
     /** The session user creator. */
-    @Inject
-    private SecurityUserCreator sessionUserCreator;
+    @SessionState(create = false)
+    private SecurityUser securityUser;
 
     /**
      * Checks if is current user has roles.
@@ -21,13 +20,11 @@ public class SecurityChecker {
      * @return true, if is current user has roles
      */
     public boolean isCurrentUserHasRoles(String[] needRoles) {
-
-        SecurityUser user = sessionUserCreator.getUserFromContext();
-        if (null != user) {
+        if (null != securityUser) {
             if (null == needRoles || 0 == needRoles.length) {
                 return true;
             }
-            return user.checkRoles(needRoles);
+            return securityUser.checkRoles(needRoles);
         }
         return false;
     }
--- src/main/java/org/gagauz/tapestry/security/SecurityModule.java
+++ src/main/java/org/gagauz/tapestry/security/SecurityModule.java
@@ -1,7 +1,5 @@
 package org.gagauz.tapestry.security;
 
-import org.gagauz.tapestry.security.impl.RedirectLoginHandler;
-
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Contribute;
@@ -9,6 +7,7 @@ import org.apache.tapestry5.ioc.annotations.Local;
 import org.apache.tapestry5.services.ComponentEventRequestFilter;
 import org.apache.tapestry5.services.PageRenderRequestFilter;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
+import org.gagauz.tapestry.security.impl.RedirectLoginHandler;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -23,7 +22,6 @@ public class SecurityModule {
      */
     public static void bind(ServiceBinder binder) {
         binder.bind(SecurityChecker.class).withId("SecurityChecker");
-        binder.bind(SecurityUserCreator.class).withId("SessionUserCreator");
         binder.bind(SecurityExceptionInterceptorFilter.class).withId("SecurityExceptionRequestFilter");
         binder.bind(LoginService.class).withId("LoginService");
         binder.bind(LogoutService.class).withId("LogoutService");
